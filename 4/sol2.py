d206dfd26fd7fa0590ba2c31499666ef7c52f915
Matuesz <bieganski.m@wp.pl>
2022-12-05 00:49:46 +0100

    4

---
+++ 4/sol2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+from pathlib import Path
+
+input = Path("input").read_text().splitlines()
+input = [x for x in input if x != ""]
+
+
+s = 0
+
+for x in input:
+    a, b = x.split(",")
+    mina, maxa = list(map(int, a.split("-")))
+    minb, maxb = list(map(int, b.split("-")))
+
+    t1 = (mina, maxa)
+    t2 = (minb, maxb)
+
+    r1 = range(mina, maxa + 1, 1)
+    r2 = range(minb, maxb + 1, 1)
+    
+    if mina in r2 or maxa in r2:
+        s += 1
+    elif minb in r1 or maxb in r1:
+        s += 1
+
+print(s)
